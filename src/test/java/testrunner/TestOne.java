b18663eec1fdf5b1644d6b5de8f593ae3acbfb19
srilatha ky <srilatha.y@thoughtworks.com>
2019-04-17 19:42:40 +0530

    My first Commit

---
+++ src/test/java/testrunner/TestOne.java
@@ -0,0 +1,14 @@
+package testrunner;
+
+import org.testng.annotations.Test;
+
+public class TestOne {
+    @Test
+    public void testone(){
+        System.out.println("This is Testng Project");
+    }
+    @Test
+    public void testtwo(){
+        System.out.println("and with jenkins Integration");
+    }
+}
